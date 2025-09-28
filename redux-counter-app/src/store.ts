4a3df3e5f8f8b61b1b7590799a12f03468816c2d
vmanidev <vmanidev@outlook.com>
2025-09-28 12:17:10 +0530

    fix: add default state to countReducer

--- redux-counter-app/src/store.ts
+++ redux-counter-app/src/store.ts
@@ -9,6 +9,7 @@ const countReducer = (state = initialState, action: Action) => {
     switch (action.type) {
         case "increment": return { ...state, count: state.count + 1 };
         case "decrement": return { ...state, count: state.count - 1 };
+        default: return state;
     }
 }
 
