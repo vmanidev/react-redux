d61ed61c003e6a1fa79e8ed88f667a1b92c5bbed
vmanidev <vmanidev@outlook.com>
2025-09-28 11:46:58 +0530

    feat(store): implement store.ts with countReducer

---
+++ redux-counter-app/src/store.ts
@@ -0,0 +1,17 @@
+import { legacy_createStore } from "redux"
+import type { Action } from "./types";
+
+const initialState = {
+    count: 0
+}
+
+const countReducer = (state = initialState, action: Action) => {
+    switch (action.type) {
+        case "increment": return { ...state, count: state.count + 1 };
+        case "decrement": return { ...state, count: state.count - 1 };
+    }
+}
+
+const store = legacy_createStore(countReducer);
+
+export default store;
