f961c818c423f17768acd2aac90375ca271e7105
vmanidev <vmanidev@outlook.com>
2025-09-28 13:55:50 +0530

    feat: connect React component to Redux store for count state management

--- redux-counter-app/src/App.tsx
+++ redux-counter-app/src/App.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import store from "./store";
+
 export default function App() {
-  return <div>App</div>;
+  const [count, setCount] = useState(store.getState().count);
+
+  useEffect(() => {
+    const subscribe = store.subscribe(() => setCount(store.getState().count));
+
+    return () => subscribe();
+  }, []);
+
+  const increment = () => {
+    store.dispatch({ type: "increment", payload: null });
+  };
+
+  const decrement = () => {
+    store.dispatch({ type: "decrement", payload: null });
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>+</button>
+      <span>{count}</span>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
