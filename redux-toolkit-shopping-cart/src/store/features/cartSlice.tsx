51811ce7ec9910df0618f9ea0b4f689a0491293c
vmanidev <vmanidev@outlook.com>
2025-10-01 01:24:46 +0530

    feat(cart): implement addItem and removeItem reducers

--- redux-toolkit-shopping-cart/src/store/features/cartSlice.tsx
+++ redux-toolkit-shopping-cart/src/store/features/cartSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { CartItem } from "../types";
 
 const initialState: CartItem[] = [];
@@ -6,7 +6,16 @@ const initialState: CartItem[] = [];
 const cartSlice = createSlice({
   name: "cart",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    addItem: (state, action: PayloadAction<CartItem>) => {
+      state.push(action.payload);
+    },
+    removeItem: (state, action: PayloadAction<CartItem>) => {
+      return state.filter(({ id }) => action.payload.id !== id);
+    },
+  },
 });
+
+export const { addItem, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
