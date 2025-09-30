03c6566825c18ade8f4cb986ae7f06c21f791ff5
vmanidev <vmanidev@outlook.com>
2025-10-01 03:19:42 +0530

    feat(products): add extraReducers for getProductList async thunk

--- redux-toolkit-shopping-cart/src/store/features/productsSlice.tsx
+++ redux-toolkit-shopping-cart/src/store/features/productsSlice.tsx
@@ -2,8 +2,9 @@ import {
   createAsyncThunk,
   createSlice,
   type ActionReducerMapBuilder,
+  type PayloadAction,
 } from "@reduxjs/toolkit";
-import type { ProductState } from "../types";
+import type { ErrorPayload, Product, ProductState } from "../types";
 
 const initialState: ProductState = {
   data: [],
@@ -20,7 +21,29 @@ const productsSlice = createSlice({
   name: "products",
   initialState: initialState,
   reducers: {},
-  extraReducers: (builder: ActionReducerMapBuilder<ProductState>) => {},
+  extraReducers: (builder: ActionReducerMapBuilder<ProductState>) => {
+    builder.addCase(getProductList.pending, (state) => {
+      state.status = "loading";
+    });
+
+    builder.addCase(
+      getProductList.fulfilled,
+      (state, action: PayloadAction<Product[]>) => {
+        state.data = action.payload;
+        state.error = null;
+        state.status = "success";
+      }
+    );
+
+    builder.addCase(
+      getProductList.rejected,
+      (state, action: PayloadAction<ErrorPayload>) => {
+        state.data = [];
+        state.error = action.payload;
+        state.status = "failed";
+      }
+    );
+  },
 });
 
 export default productsSlice.reducer;
