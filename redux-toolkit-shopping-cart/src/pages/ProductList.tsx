979bff53b8142c333fc4909b40c66fdd3ff9631c
vmanidev <vmanidev@outlook.com>
2025-10-01 13:01:38 +0530

    feat(ui): add Tailwind CSS classes to UI components

--- redux-toolkit-shopping-cart/src/pages/ProductList.tsx
+++ redux-toolkit-shopping-cart/src/pages/ProductList.tsx
@@ -15,18 +15,31 @@ export default function ProductList() {
   const fetchProducts = () => {
     return data.map((product) => {
       return (
-        <li key={product.id}>
-          <figure>
-            <img src={product.image} />
-            <figcaption>{product.title}</figcaption>
+        <li
+          key={product.id}
+          className="flex flex-col bg-white border border-gray-200 rounded-xl shadow-sm p-4 transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:shadow-lg"
+        >
+          <figure className="m-0 fkex flex-col text-left justify-center">
+            <img
+              className="max-w-full h-[180px] object-contain rounded-md mb-2 justify-self-center"
+              src={product.image}
+            />
+            <figcaption>
+              <div className="block text-xs italic text-gray-400">
+                {product.category}
+              </div>
+              <div className="text-md font-semibold mb-2 text-gray-900">
+                {product.title}
+              </div>
+            </figcaption>
           </figure>
-          <div>
-            <span>{product.description}</span>
-            <span>{product.category}</span>
-          </div>
-          <div>
-            <span>{product.price}</span>
-            <button>Add to cart</button>
+          <div className="flex justify-between items-center mt-auto">
+            <span className="text-lg font-bold text-gray-900">
+              &#8377; {product.price}
+            </span>
+            <button className="bg-blue-600 text-white px-4 py-2 text-sm rounded-md transition-colors duration-200 hover:bg-blue-900 cursor-pointer">
+              Add to cart
+            </button>
           </div>
         </li>
       );
@@ -35,11 +48,21 @@ export default function ProductList() {
 
   return (
     <div>
-      <h2>Products</h2>
+      <h2 className="text-3xl font-bold text-center text-gray-800 m-6">
+        Products
+      </h2>
       {status === "loading" ? (
-        <div>Fetching all products...</div>
+        <div className="text-md text-center text-blue-600">
+          Fetching all products...
+        </div>
+      ) : status === "success" ? (
+        <ul className="list-none p-0 m-0 grid [grid-template-columns:repeat(auto-fill,minmax(240px,1fr))] gap-6">
+          {fetchProducts()}
+        </ul>
       ) : (
-        <ul>{fetchProducts()}</ul>
+        <div className="text-red-600 text-md mt-1 font-medium text-center italic">
+          Error: {error?.message}
+        </div>
       )}
     </div>
   );
